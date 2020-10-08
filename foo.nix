b14e66605c2d5c36085b5fe8d0931c4b32ed8783
Tethys Svensson <freaken@freaken.dk>
2020-10-08 15:33:31 +0200

    With c++ headers

--- foo.nix
+++ foo.nix
@@ -1,4 +1,4 @@
-{ gccTest, stdenv, wrapCC }:
+{ gccTest, stdenv, wrapCC, libcxxStdenv }:
 
 let cc = wrapCC gccTest;
 in
@@ -6,6 +6,9 @@ in
 stdenv.mkDerivation {
   name = "foo";
   src = ./foo.c;
+
+  buildInputs = [ libcxxStdenv ];
+
   phases = [ "buildPhase" ];
   buildPhase = ''
     ${cc}/bin/g++ -O3 -fopenmp -fno-stack-protector -foffload=-lm -foffload="-misa=sm_35" -xc $src -o $out
