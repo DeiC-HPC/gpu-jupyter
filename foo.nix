854a11766c1a350e3e227904bf46634a16db4b35
Tethys Svensson <freaken@freaken.dk>
2020-10-08 15:48:39 +0200

    Does this work then?

--- foo.nix
+++ foo.nix
@@ -4,8 +4,6 @@ stdenv.mkDerivation {
   name = "foo";
   src = ./foo.cpp;
 
-  buildInputs = [ libstdcxx5 ];
-
   phases = [ "buildPhase" ];
   buildPhase = ''
     ${gccTest}/bin/g++ -O3 -fopenmp -fno-stack-protector -foffload=-lm -foffload="-misa=sm_35" -xc $src -o $out
