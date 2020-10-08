45ab516d74ff4d080ad4f5ac71569c1fe9ce8200
Tethys Svensson <freaken@freaken.dk>
2020-10-08 15:30:31 +0200

    Actual code!

--- foo.nix
+++ foo.nix
@@ -5,8 +5,9 @@ in
 
 stdenv.mkDerivation {
   name = "foo";
+  src = ./foo.c
   phases = [ "buildPhase" ];
   buildPhase = ''
-    echo 'int main(){}' | ${cc}/bin/gcc -xc - -o $out
+    ${cc}/bin/g++ -O3 -fopenmp -fno-stack-protector -foffload=-lm -foffload="-misa=sm_35" -xc $src -o $out
   '';
 }
