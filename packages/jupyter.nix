b0df0230023a94129719844dd7d6684b84d1f9a8
Tethys Svensson <freaken@freaken.dk>
2020-10-20 08:33:29 +0200

    Make a better wrapper

--- packages/jupyter.nix
+++ packages/jupyter.nix
@@ -1,9 +1,12 @@
-{ makeWrapper, symlinkJoin, jupyter, cudatoolkit, linuxPackages }:
-symlinkJoin {
-  name = "jupyter";
-  paths = [ jupyter ];
-  buildInputs = [ makeWrapper ];
-  postBuild = ''
-    wrapProgram $out/bin/jupyter-lab --prefix PATH : "${cudatoolkit}/bin" --prefix LD_LIBRARY_PATH : "${linuxPackages.nvidia_x11}/lib"
-  '';
-}
+{ coreutils, writeScriptBin, jupyter, cudatoolkit, linuxPackages, bash }:
+writeScriptBin "jupyter-lab" ''
+  #!${bash}/bin/bash
+
+  set -ev
+
+  export NIX_CC_WRAPPER_TARGET_HOST_x86_64_unknown_linux_gnu=1
+  export NIX_HARDENING_ENABLE=fortify stackprotector pic strictoverflow relro bindnow
+  export LD_LIBRARY_PATH="${linuxPackages.nvidia_x11}/lib"
+
+  exec ${jupyter}/bin/jupyter-lab "$@"
+''
