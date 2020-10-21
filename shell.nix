402a4a2eeec2d34837351a8196bbac5d10232001
Tethys Svensson <freaken@freaken.dk>
2020-10-21 16:04:08 +0200

    Fix up variables in nix-shell

--- shell.nix
+++ shell.nix
@@ -7,4 +7,8 @@ with (import ./common/outputs.nix {
 
 pkgs.mkShell {
   nativeBuildInputs = [ jupyter gcc10Offloading ];
+  shellHook = ''
+    export NIX_CC_WRAPPER_TARGET_HOST_x86_64_unknown_linux_gnu=1
+    export NIX_HARDENING_ENABLE='fortify stackprotector pic strictoverflow relro bindnow'
+  '';
 }
