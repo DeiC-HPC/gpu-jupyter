416e0a2f5b0482e9b6c970fe6215ebd7158dd509
TheBlackKoala <torskovsgaard@hotmail.com>
2026-04-28 11:38:39 +0200

    First pass

--- packages/nvhpc.nix
+++ packages/nvhpc.nix
@@ -8,23 +8,24 @@ let
       zlib
     ]);
   };
+  version = "26.3";
 
   installNvhpc = pkgs.writeScript "install-nvhpc" ''
     #!${pkgs.bash}/bin/bash
     export NVHPC_SILENT=true;
     export NVHPC_INSTALL_DIR=$out;
-    sed 's@grep include@grep "^\\s*/\\S*include"@' -i ./install_components/Linux_x86_64/20.11/compilers/bin/makelocalrc
+    sed 's@grep include@grep "^\\s*/\\S*include"@' -i ./install_components/Linux_x86_64/${version}/compilers/bin/makelocalrc
     ./install
   '';
 
   nvhpcInstallation = pkgs.stdenv.mkDerivation rec {
     pname = "nvidia-hpc-sdk";
-    version = "20.11";
+    version = "${version}";
 
     phases = [ "unpackPhase" "installPhase" ];
 
     src = fetchurl {
-      url = "https://developer.download.nvidia.com/hpc-sdk/20.11/nvhpc_2020_2011_Linux_x86_64_cuda_11.1.tar.gz";
+      url = "https://developer.download.nvidia.com/hpc-sdk/${version}/nvhpc_2026_263_Linux_x86_64_cuda_13.1.tar.gz";
       sha256 = "0njqsflw59wv0hgdgib1f1f1qn2kvzarcyycm7gzzc5nls55vfpf";
     };
 
@@ -36,8 +37,8 @@ let
       ${installEnv}/bin/${installEnv.name}
     '';
   };
-  nvcpp = userEnv "nvc++" "${nvhpcInstallation}/Linux_x86_64/20.11/compilers/bin/nvc++";
-  nvfortran = userEnv "nvfortran" "${nvhpcInstallation}/Linux_x86_64/20.11/compilers/bin/nvfortran";
+  nvcpp = userEnv "nvc++" "${nvhpcInstallation}/Linux_x86_64/${version}/compilers/bin/nvc++";
+  nvfortran = userEnv "nvfortran" "${nvhpcInstallation}/Linux_x86_64/${version}/compilers/bin/nvfortran";
 in
   pkgs.symlinkJoin {
     name = "nvidia-hpc-compilers";
