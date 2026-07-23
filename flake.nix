2135e41be14f0c809b041b8cbb4044a856970726
TheBlackKoala <torskovsgaard@hotmail.com>
2026-07-23 10:44:54 +0200

    Poetry problems

--- flake.nix
+++ flake.nix
@@ -16,6 +16,11 @@
     
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     jupyenv = {
       url = "github:tweag/jupyenv";
       inputs.nixpkgs.follows = "nixpkgs";
